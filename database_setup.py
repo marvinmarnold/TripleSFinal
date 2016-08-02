213093ed1e876f37537e22324c61799221f33cf1
Marvin Arnold <marvinmarnold@gmail.com>
2016-08-02 09:05:31 +0300

    Import OS

--- database_setup.py
+++ database_setup.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker, scoped_session
 from sqlalchemy import create_engine
+import os
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
